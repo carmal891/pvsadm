2ed66b938ea573913da1ebce86d2e00d6e055720
carmal891 <carmal911@gmail.com>
2024-11-16 21:12:37 +0530

    test homebrew updagte workflow

--- ci/brew_formula_updater.sh
+++ ci/brew_formula_updater.sh
@@ -34,7 +34,7 @@ BREW_TAP_REPO_URL="$3"
 FORMULA_FILE="$4"
 FORMULA_PATH="Formula/"
 #RELEASES_PATH="$BASE_REPO_URL/releases/download"
-RELEASES_PATH=https://github.com/ppc64le-cloud/pvsadm/releases/download
+RELEASES_PATH="https://github.com/ppc64le-cloud/pvsadm/releases/download"
 
 
 echo "BASE_REPO_URL: $BASE_REPO_URL"
