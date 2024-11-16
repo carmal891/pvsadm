02ba61733509e5762062b0bfcaf1fd664516d892
carmal891 <carmal911@gmail.com>
2024-11-16 21:50:02 +0530

    update homebrew formula v2

--- ci/brew_formula_updater.sh
+++ ci/brew_formula_updater.sh
@@ -33,7 +33,7 @@ NEW_VERSION="$2"
 BREW_TAP_REPO_URL="$3"
 FORMULA_FILE="$4"
 FORMULA_PATH="Formula/"
-#RELEASES_PATH="$BASE_REPO_URL/releases/download"
+##RELEASES_PATH="$BASE_REPO_URL/releases/download"
 RELEASES_PATH="https://github.com/ppc64le-cloud/pvsadm/releases/download"
 
 
